e20a21615c123695acad100acfc13077c770a112
Michael Welford <mail@michaelwelford.com>
2026-04-13 08:14:36 +0930

    feat: add in global logger.

--- main.go
+++ main.go
@@ -12,12 +12,15 @@ import (
 	"boot.dev/linko/internal/store"
 )
 
+var logger *log.Logger
+
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 
 	httpPort := flag.Int("port", 8899, "port to listen on")
 	dataDir := flag.String("data", "./data", "directory to store data")
 	flag.Parse()
+	logger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
 
 	status := run(ctx, cancel, *httpPort, *dataDir)
 	cancel()
@@ -27,10 +30,10 @@ func main() {
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
 	st, err := store.New(dataDir)
 	if err != nil {
-		log.Printf("failed to create store: %v", err)
+		logger.Printf("failed to create store: %v", err)
 		return 1
 	}
-	log.Printf("Linko is running on http://localhost:%d", httpPort)
+	logger.Printf("Linko is running on http://localhost:%d", httpPort)
 	s := newServer(*st, httpPort, cancel)
 	var serverErr error
 	go func() {
@@ -42,14 +45,14 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		log.Printf("failed to shutdown server: %v", err)
+		logger.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
 	if serverErr != nil {
-		log.Printf("server error: %v", serverErr)
+		logger.Printf("server error: %v", serverErr)
 		return 1
 	}
 
-	log.Printf("Linko is shutting down")
+	logger.Printf("Linko is shutting down")
 	return 0
 }
